6ba61491af3d344da50db297c4f80ed3ac29b3be
Daniil <danilmux@gmail.com>
2024-02-18 20:09:34 +0500

    bigInt

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+// const bigInt = 123123123123312983129849120481249012904120498n;
+
+const sameBigInt = BigInt(123123123123312983129849120481249012904120498);
+
+// console.log(typeof (bigInt));
+
+let bigInt = 1n;
+
+let number = 2;
+
+console.log(bigInt + BigInt(number));
+console.log(Number(bigInt) + number);
